ea3dc922db558c0286ed4a21ce5fa906d416e392
Victoria <vmichellej@gmail.com>
2017-06-05 10:58:23 -0400

    Recordcount null when not given (#5)

--- src/core/reducers/data/record-count.ts
+++ src/core/reducers/data/record-count.ts
@@ -2,7 +2,7 @@ import Actions from '../../actions';
 
 export type State = number;
 
-export default function updateRecordCount(state: State = 0, action): State {
+export default function updateRecordCount(state: State = null, action): State {
   switch (action.type) {
     case Actions.RECEIVE_RECORD_COUNT: return action.recordCount;
     default: return state;
