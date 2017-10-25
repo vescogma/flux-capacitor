219992c0136acfbeb162e46268587f48e6df84c0
Bruce Chen <bruce.chen@groupbyinc.com>
2017-10-25 15:04:10 -0400

    Add file

---
+++ src/core/storage-manager.ts
@@ -0,0 +1,10 @@
+import Configuration from './configuration';
+import Store from './store';
+
+namespace StorageManager {
+    export const update = () => {
+        //todo
+    };
+}
+
+export default StorageManager;
