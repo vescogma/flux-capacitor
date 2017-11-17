68b3a2caa94b9a9fcc8b1a99b49e485b71a97499
Mengqi <mengqi.zhu@groupbyinc.com>
2017-11-17 12:30:45 -0500

    wip: create cart

---
+++ src/core/sagas/cart.ts
@@ -0,0 +1,36 @@
+import * as effects from 'redux-saga/effects';
+import FluxCapacitor from '../../flux-capacitor';
+import Actions from '../actions';
+import Adapter from '../adapters/autocomplete';
+import ConfigAdapter from '../adapters/configuration';
+import Configuration from '../configuration';
+import Requests from '../requests';
+import Selectors from '../selectors';
+import Store from '../store';
+import { fetch } from '../utils';
+
+export namespace Tasks {
+  export function* createCart(flux: FluxCapacitor, { payload: { loginId, sessionId, visitorId }}: Actions.CreateCart) {
+    try {
+      const request = yield effects.select(Requests.autocompleteProducts);
+      const res = yield effects.call(
+        // [flux.clients.bridge, flux.clients.bridge.search],
+        {
+          ...request,
+          loginId,
+          sessionId,
+          visitorId
+        }
+      );
+
+      // yield effects.put(<any>flux.actions.receiveAutocompleteProducts(res));
+    } catch (e) {
+      // yield effects.put(<any>flux.actions.receiveAutocompleteProducts(e));
+    }
+  }
+}
+
+export default (flux: FluxCapacitor) => function* cartSaga() {
+  // yield effects.takeLatest(Actions.FETCH_AUTOCOMPLETE_SUGGESTIONS, Tasks.fetchSuggestions, flux);
+  // yield effects.takeLatest(Actions.FETCH_AUTOCOMPLETE_PRODUCTS, Tasks.fetchProducts, flux);
+};
