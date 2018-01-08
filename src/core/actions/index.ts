fc1d1ece1c9203fe0a8e6e57655b9c966a608857
Victoria Johns <vmichellej@gmail.com>
2018-01-08 15:40:05 -0500

    Cleanup

--- src/core/actions/index.ts
+++ src/core/actions/index.ts
@@ -1,7 +1,7 @@
 import { Results } from 'groupby-api';
-import Payloads from './payloads';
 import Configuration from '../configuration';
 import Store from '../store';
+import Payloads from './payloads';
 
 namespace Actions {
   export interface Action<S = string, T = any> {
@@ -208,98 +208,6 @@ namespace Actions {
   export type RefreshState = Action<typeof REFRESH_STATE, any>;
   // added automatically by middleware to interact with redux-undo
   export const SAVE_STATE = 'SAVE_STATE';
-
-  export namespace Payload {
-    export namespace Component {
-      export interface Identifier {
-        tagName: string;
-        id: string;
-      }
-
-      export interface State extends Identifier {
-        state: object;
-        persist: boolean;
-      }
-    }
-
-    export namespace Collection {
-      export interface Count {
-        collection: string;
-        count: number;
-      }
-    }
-
-    export interface Query {
-      corrected?: string;
-      related: string[];
-      didYouMean: string[];
-      rewrites: string[];
-    }
-
-    // NOTE: Isn't getting the right type in generated doc for some reason
-    export interface Search extends Partial<Navigation.Refinement>, Partial<Navigation.AddRefinement> {
-      query?: string;
-
-      /**
-       * only for refinements
-       * if true, replace refinements with the provided ones
-       * if false, add the provided refinements
-       */
-      clear?: boolean | string;
-    }
-
-    export namespace Autocomplete {
-      export interface Suggestions {
-        suggestions: Store.Autocomplete.Suggestion[];
-        categoryValues: string[];
-        navigations: Store.Autocomplete.Navigation[];
-      }
-
-      export interface Refinement {
-        field: string;
-        value: string;
-      }
-    }
-
-    export namespace Navigation {
-      export interface Refinement {
-        navigationId: string;
-        index: number;
-      }
-
-      export interface AddRefinement {
-        navigationId: string;
-        range?: boolean;
-
-        // used to add new value refinement
-        value?: string;
-
-        // used to add new range refinement
-        low?: number;
-        high?: number;
-      }
-
-      export interface MoreRefinements {
-        navigationId: string;
-        refinements: Store.Refinement[];
-        selected: number[];
-      }
-    }
-
-    export interface Page {
-      previous: number;
-      next: number;
-      last: number;
-      from: number;
-      to: number;
-      current?: number;
-    }
-
-    export interface InfiniteScroll {
-      isFetchingForward?: boolean;
-      isFetchingBackward?: boolean;
-    }
-  }
 }
 
 export default Actions;
