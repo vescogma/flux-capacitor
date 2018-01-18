32590ae88f178079bcc05b0d7bc9b8fb8b9cbf90
mengqi <treeforever7@gmail.com>
2018-01-18 17:39:45 -0500

    add a flag for history

--- src/core/selectors.ts
+++ src/core/selectors.ts
@@ -522,7 +522,6 @@ namespace Selectors {
           refs.concat(nav.range
             ? { navigationName: nav.field, type: 'Range', high, low }
             : { navigationName: nav.field, type: 'Value', value }), [])), []);
-
 }
 
 export default Selectors;
